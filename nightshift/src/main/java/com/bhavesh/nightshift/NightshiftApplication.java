7c17867c8fd657cc67234e36ed8d31ada35dc68a
bhpesw <bhpesw@taltech.ee>
2026-09-07 23:46:40 +0530

    Initial Spring Boot project

---
+++ nightshift/src/main/java/com/bhavesh/nightshift/NightshiftApplication.java
@@ -0,0 +1,13 @@
+package com.bhavesh.nightshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightshiftApplication.class, args);
+	}
+
+}
---
+++ nightshift/src/test/java/com/bhavesh/nightshift/NightshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavesh.nightshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
